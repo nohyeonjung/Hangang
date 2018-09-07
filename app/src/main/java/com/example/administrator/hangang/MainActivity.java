c0e1105fa9309ed65d81bdf3defc552962091d84
paeroo <paeroo0304@naver.com>
2018-09-07 16:21:52 +0900

    툴바 추가 테스트1

--- app/src/main/java/com/example/administrator/hangang/MainActivity.java
+++ app/src/main/java/com/example/administrator/hangang/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.administrator.hangang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //pull test
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu_icon);
+        getSupportActionBar().setTitle("");
     }
+
+
 }
